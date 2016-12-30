d502b8e91e76422b2fcc5276b755bfe248b55a49
Alexandre Resende <alexandre.amres@gmail.com>
2016-12-30 21:40:29 -0200

    Exercises done on December 30th 2016

---
+++ URI_Online_Judge/1075.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main(void){
+
+	int n, ans;
+	scanf("%d", &n);
+
+	for (ans = 1; ans <= 10000; ans++){
+		if (ans % n == 2){
+			printf("%d\n", ans);
+		}
+	}
+
+	return 0;
+}
