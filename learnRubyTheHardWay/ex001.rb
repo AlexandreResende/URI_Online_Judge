cbb3a4e71d66bfdd137711093d5f5a6425ef3804
Alexandre Resende <alexandre.amres@gmail.com>
2017-04-14 08:46:43 -0300

    Exercises 1 - 2

---
+++ learnRubyTheHardWay/ex001.rb
@@ -0,0 +1,6 @@
+#this is a comment in ruby
+puts "Hello World in Ruby!"
+puts "Practicing a new language"
+#puts "It's seems weird"
+puts 'It\'s actually printing something here'
+#puts 'cool'
---
+++ learnRubyTheHardWay/ex002.rb
@@ -0,0 +1,9 @@
+# A comment, this is so you can read yuor program later
+#Anything after # is ignored in ruby
+
+puts "I could have code like this"
+
+#You can also use comment to "disable" or comment out a piece of code:
+#puts "thos won't work"
+
+puts "This will run"
